f02c7ce0e228b8cdd7ea36e8d91764e33bdf6569
frixie <nikitka.6525@gmail.com>
2021-02-25 03:36:13 +0300

    add wiki code (eng lib)

--- ds_bot.py
+++ ds_bot.py
@@ -1,9 +1,39 @@
 import discord
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print("Logged on as {0}!".format(self.user))
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+import os
+import datetime
+import random
+from random import randint
+import wikipedia
 
-    async def on_message(self, message):
-        print("Message from {0.author}: {0.content}".format(message))
-client=MyClient()
-client.run('ODEzNTQ1MTI0NTQxMjM1MjAx.YDQ28A.TeRqMbTekVvnigr8mJKI4dStRwo')
+client = commands.Bot(command_prefix = '!')
+
+owner_id = 383623196894822400
+chan_id = 813544820991197197
+
+token = 'ODEzNTQ1MTI0NTQxMjM1MjAx.YDQ28A.Rek0fTl9-BVP0Alw0l3AU8AVr48'
+
+@client.event
+async def on_ready():
+    time = datetime.datetime.now()
+    print('WikiBot online at {}'.format(time))
+
+def wiki_summary(arg):
+    definition = wikipedia.summary(arg, sentences=3, chars=1000,
+    auto_suggest=True, redirect=True)
+    return definition
+
+@client.event
+async def on_message('wikipedia.com/' + message):
+    words = message.content.split()
+    important_words = words[1:]
+
+    if message.content.startswith('!search'):
+        words = message.content.split()
+        important_words = words[1:]
+        search = discord.Embed(title='Successful search...', description=wiki_summary(important_words), color=discord.Color.purple())
+        await message.channel.send(content=None, embed=search)
+
+client.run(token)
