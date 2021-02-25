3ad86ea68f3f87323f6b6455a24e7dd4ccecf548
frixie <nikitka.6525@gmail.com>
2021-02-25 14:51:19 +0300

    fix env +add ru lib

--- ds_bot.py
+++ ds_bot.py
@@ -1,26 +1,27 @@
-import discord
-from discord.ext import commands
-from discord.ext.commands import Bot
-import asyncio
-import os
 import datetime
-import random
-from random import randint
+
+import discord
 import wikipedia
+from discord.ext import commands
 
-client = commands.Bot(command_prefix = '!')
+wikipedia.set_lang('ru')
 
-token = ''
+client = commands.Bot(command_prefix='!')
+
+token = 'ODEzNTQ1MTI0NTQxMjM1MjAx.YDQ28A.x-1IdrxurwkQgKA3MHKd31R_z8s'
+
 
 @client.event
 async def on_ready():
     time = datetime.datetime.now()
     print('WikiBot online at {}'.format(time))
 
+
 def wiki_summary(arg):
     definition = wikipedia.summary(arg, sentences=3, chars=1000,
-    auto_suggest=True, redirect=True)
+                                   auto_suggest=True, redirect=True)
     return definition
+
 
 @client.event
 async def on_message(message):
@@ -30,7 +31,9 @@ async def on_message(message):
     if message.content.startswith('!search'):
         words = message.content.split()
         important_words = words[1:]
-        search = discord.Embed(title='Successful search...', description=wiki_summary(important_words), color=discord.Color.purple())
+        search = discord.Embed(title='Successful search...', description=wiki_summary(important_words),
+                               color=discord.Color.purple())
         await message.channel.send(content=None, embed=search)
+
 
 client.run(token)
